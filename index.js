d6d0baeeb9f23632ab80610d0e77dc781618fac5
xavsln <19362582+xavsln@users.noreply.github.com>
2022-08-09 15:10:23 +0200

    Implement eslintrc

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
-const mongoose = require("mongoose");
-const Models = require("./models.js");
+const mongoose = require('mongoose');
+const Models = require('./models.js');
 
 const Monuments = Models.Monument;
 
-mongoose.connect("mongodb://localhost:27017/frenchStateHistoricalMonumentsDB", {
+mongoose.connect('mongodb://localhost:27017/frenchStateHistoricalMonumentsDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -16,14 +16,14 @@ mongoose.connect("mongodb://localhost:27017/frenchStateHistoricalMonumentsDB", {
 // GET routes
 // ========================
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-  res.send("Welcome to the French State Historical Monuments API!");
+app.get('/', (req, res) => {
+  res.send('Welcome to the French State Historical Monuments API!');
 });
 
 // READ - Returns a list of all Monuments
-app.get("/monuments", (req, res) => {
+app.get('/monuments', (req, res) => {
   // res.send("This should render the list of all monuments available in the API");
   Monuments.find()
     .then(monuments => {
@@ -31,29 +31,29 @@ app.get("/monuments", (req, res) => {
     })
     .catch(err => {
       console.error(err);
-      res.status(500).send("Error: " + err);
+      res.status(500).send('Error: ' + err);
     });
 });
 
 // READ - Returns data in JSON format of specific Monument by monumentId
-app.get("/monuments/:monumentId", (req, res) => {
+app.get('/monuments/:monumentId', (req, res) => {
   // res.send("This should render data of a specific monument");
   Monuments.findOne({ _id: req.params.monumentId })
     .then(monument => {
       if (monument) {
         res.status(200).json(monument);
       } else {
-        res.status(400).send("No such a monument in the database.");
+        res.status(400).send('No such a monument in the database.');
       }
     })
     .catch(err => {
       console.error(err);
-      res.status(500).send("Error: " + err);
+      res.status(500).send('Error: ' + err);
     });
 });
 
 // READ - Returns data in JSON format of all monuments located in a given city
-app.get("/monuments/locations/:cityLocation", (req, res) => {
+app.get('/monuments/locations/:cityLocation', (req, res) => {
   // res.send("This should render the list of all monuments available in the API");
   Monuments.find({ CityLocation: req.params.cityLocation })
     .then(monuments => {
@@ -63,7 +63,7 @@ app.get("/monuments/locations/:cityLocation", (req, res) => {
     })
     .catch(err => {
       console.error(err);
-      res.status(500).send("Error: " + err);
+      res.status(500).send('Error: ' + err);
     });
 });
 
