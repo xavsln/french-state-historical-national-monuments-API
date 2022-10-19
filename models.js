0ef2b453bfab30dde6d7d772884296a3f44edb48
xavsln <19362582+xavsln@users.noreply.github.com>
2022-10-19 19:23:17 +0200

    Add thumbImgUrl to monumentSchema

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 let monumentSchema = mongoose.Schema({
   name: { type: String, required: true },
@@ -10,7 +10,12 @@ let monumentSchema = mongoose.Schema({
   cityLocation: { type: String, required: true },
   departmentLocation: { type: String, required: true },
   regionLocation: { type: String, required: true },
-  remarks: String
+  thumbImgUrl: {
+    type: String,
+    required: true,
+    default: "https://via.placeholder.com/250",
+  },
+  remarks: String,
 });
 
 let userSchema = mongoose.Schema({
@@ -19,21 +24,21 @@ let userSchema = mongoose.Schema({
   email: { type: String, required: true },
   birthday: Date,
   favoriteMonuments: [
-    { type: mongoose.Schema.Types.ObjectId, ref: 'Monument' }
+    { type: mongoose.Schema.Types.ObjectId, ref: "Monument" },
   ],
-  role: { type: String, enum: ['user', 'admin'], default: 'user' }
+  role: { type: String, enum: ["user", "admin"], default: "user" },
 });
 
-userSchema.statics.hashPassword = password => {
+userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = function(password) {
+userSchema.methods.validatePassword = function (password) {
   return bcrypt.compareSync(password, this.password);
 };
 
-let Monument = mongoose.model('Monument', monumentSchema);
-let User = mongoose.model('User', userSchema);
+let Monument = mongoose.model("Monument", monumentSchema);
+let User = mongoose.model("User", userSchema);
 
 module.exports.Monument = Monument;
 module.exports.User = User;
